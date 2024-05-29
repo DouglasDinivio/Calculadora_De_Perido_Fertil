02d3aed31c9bb34b705c30b4f035b84bfe22f73a
Douglas <SrBila@hotmail.com>
2024-05-29 02:08:18 +0200

    Adiçao de logica de calculo e melhoramento da aplicaçao, as duas views encontram-se operacionais, desepnenhando como uma um papel, uma de form para coleta de dados de usuario e outra para apresentar resultados de manipulaçao de dados inseriod dando resultados ao client

--- app/Http/Controllers/welcomeController.php
+++ app/Http/Controllers/welcomeController.php
@@ -7,16 +7,42 @@ use Illuminate\Http\Request;
 class welcomeController extends Controller
 {
     //
-    public function formulario(Request $request){
+    public function formulario(Request $request)
+    {
+
+        /*    $dados = $request->all();
+
+            $IUPM =  $dados['IUPM'];
+            $DMCM = $dados['DMCM'];
+        */
+        $IUPM = $request->query('IUPM');
+        $DMCM = $request->query('DMCM');
+        $tipo = "em dias";
+        $CálculoDiaOvulação = $DMCM - 14+1;
+
+        //$CálculoPeríodoFértil = $IUPM-5; 
+        $data1 = $IUPM;
+        $data2 =date('Y-m-d');
         
-    /*    $dados = $request->all();
+        $timestamp1 = strtotime($data1);
+        $timestamp2 = strtotime($data2);
+        $diferencaSegundos = abs($timestamp1 - $timestamp2);
+        $CálculoPeríodoFértil = $diferencaSegundos / (60 * 60 * 24);
 
-        $IUPM =  $dados['IUPM'];
-        $DMCM = $dados['DMCM'];
-    */
-    $IUPM = $request->query('IUPM');
-    $DMCM = $request->query('DMCM');
+         $PrimeiroDiaPeriodoFertil = $CálculoDiaOvulação - 5;
 
-        return view('welcome', ['IUPM'=>$IUPM, 'DMCM'=>$DMCM]);
+        $ÚltimoDiaPeriodoFertil = $CálculoDiaOvulação + 1 ;
+        return view(
+            'welcome',
+            [
+                'IUPM' => $IUPM,
+                'DMCM' => $DMCM,
+                'CálculoDiaOvulação' => $CálculoDiaOvulação,
+                'Dias' => $CálculoPeríodoFértil, 
+                'PrimeiroDiaPeriodoFertil' =>$PrimeiroDiaPeriodoFertil,
+                'ÚltimoDiaPeriodoFertil'=> $ÚltimoDiaPeriodoFertil
+            ]
+        );
+
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo de Centralização</title>
+    <title>CPF - Calculadora de Periodo Fertil</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <style>
         * {
             margin: 0;
+
         }
 
         .center-content {
@@ -32,6 +33,7 @@
 
         @media (max-width: 576px) {
             .nav-tabs {
+                margin-top: 150px;
                 padding: 10px;
             }
         }
@@ -55,12 +57,14 @@
 </head>
 
 <body
-    style="background: rgb(238,174,202); background: linear-gradient(0deg, rgba(238,174,202,0.48783263305322133) 0%, rgba(148,187,233,0.4906337535014006) 100%);">
+    style="background: rgb(238,174,202); background: linear-gradient(180deg, rgba(238,174,202,0.48783263305322133) 100%, rgba(148,187,233,0.4906337535014006) 0%); height:100vh;">
 
-    <ul class="nav nav-tabs">
-        <img src="{{ asset('images/menstrual-logo.png') }}" alt="menstrual_logo" style="width: 150px"
+    <ul class="nav nav-tabs mt-4">
+        
+        <img src="{{ asset('images\menstrual-logo.png') }}" alt="menstrual_logo" style="width: 150px"
             class="mx-auto d-block">
     </ul>
+    <br>
 
     <form method="get">
         @csrf
@@ -68,7 +72,7 @@
         <div class="center-container">
             <div class="center-content">
                 <div class="col-12">
-                    <label for="dateInput"><b>IUPM</b></label>
+                    <label for="dateInput"><b>Inicio da última menstruação:</b></label>
                 </div>
                 <div class="input-group mb-4">
                     <input type="date" class="form-control" aria-label="Sizing example input" name="IUPM"
@@ -76,10 +80,10 @@
                 </div>
 
                 <div class="col-12">
-                    <label for="dateInput"><b>DMCM</b></label>
+                    <label for="dateInput"><b>Duração do ciclo menstrual em dias:</b></label>
                 </div>
                 <div class="input-group mb-4">
-                    <input type="number" class="form-control" aria-label="Sizing example input" name="DMCM"
+                    <input type="#" class="form-control" aria-label="Sizing example input" name="DMCM"
                         aria-describedby="inputGroup-sizing-default">
                 </div>
 
@@ -87,14 +91,41 @@
                     <button type="submit" class="btn btn-red form-control"><b>Calcular</b></button>
                 </div>
 
-                <h3></h3>
-                <label> <b>IUPM :</b> {{ $IUPM }}</label>
-                <label> <b>IUPM :</b> {{ $DMCM }}</label>
+<br>
+                <label><b>Resultado:</b></label>
+
+                <small style="text-align: center">
+                    <label><b style="font-weight: 600">Dia da Ovulação : </b> {{ $CálculoDiaOvulação }}</label><br>
+                    O seu dia de ovulação é o dia {{ $CálculoDiaOvulação }}, neste dia que durante o ciclo
+                    menstrual da mulher o ovário
+                    libera um óvulo. Este é o momento em que a
+                    mulher é mais fértil e tem a maior
+                    probabilidade de engravidar se houver
+                    relação sexual desprotegida.
+                </small>
+<br>
+
+
+                <small style="text-align: center">
+                    <label><b style="font-weight: 600">Período Fértil </b></label> <br>
+                    <label><b>Primeiro dia do período fértil :</b> {{ $PrimeiroDiaPeriodoFertil }}</label>
+                    <label><b>Último dia do período fértil :</b>{{ $ÚltimoDiaPeriodoFertil }}</label> <br>    
+                    O Período Fértil é a janela de tempo no ciclo menstrual em que a mulher tem a maior chance de
+                    engravidar. Ele geralmente inclui o dia da ovulação e os cinco dias anteriores, totalizando cerca de
+                    seis dias. Durante esse período, os espermatozoides podem sobreviver no corpo da mulher por até
+                    cinco dias, e o óvulo permanece viável por cerca de 24 horas após a ovulação
+                </small>
+                <label> </label>
+                <label> </label>
             </div>
         </div>
     </form>
+    <div class="center-container">
+        <div class="center-content">
+        <footer><small>@2024</small></footer>
+        </div>
+    </div>
 
-  
 </body>
 
 </html>
